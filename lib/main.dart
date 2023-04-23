4bb40452ead4ecc0f3ed34faf80b72088d091200
Luan Simão <luan.simao@fatec.sp.gov.br>
2023-04-22 23:29:59 -0300

    ListagemAnimais

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:teste_navbar/ButtonCustom.dart';
+//import 'package:teste_navbar/ButtonCustom.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,14 +11,137 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      theme: ThemeData(),
+      home: Scaffold(
+        backgroundColor:
+            Color(0xFFFAFAFA), // aqui você pode definir a cor de fundo
+        body: ListagemAnimais(),
       ),
-      home: ModalSheet(),
     );
   }
 }
 
+class ListagemAnimais extends StatefulWidget {
+  @override
+  _ListagemAnimaisState createState() => _ListagemAnimaisState();
+}
+
+class _ListagemAnimaisState extends State<ListagemAnimais> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(8)),
+        color: Color(0xFFF0F0F0),
+      ),
+      margin: EdgeInsets.all(32),
+      height: 100,
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Container(
+                padding: EdgeInsets.all(10),
+                alignment: Alignment.center,
+                width: 64,
+                height: 64,
+                child: const CircleAvatar(
+                  backgroundImage:
+                      AssetImage('../assets/images/Icon_cowprod.png'),
+                  backgroundColor: Color(0xFFFCFCFC),
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  children: [
+                    Container(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 10, vertical: 2),
+                      alignment: Alignment.topLeft,
+                      child: const Text(
+                        'Ben',
+                        style: TextStyle(
+                          fontFamily: 'Axiforma',
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF0C1E10),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 10, vertical: 2),
+                      alignment: Alignment.topLeft,
+                      child: const Text(
+                        '#38724951',
+                        style: TextStyle(
+                          fontFamily: 'Axiforma',
+                          fontSize: 12,
+                          fontWeight: FontWeight.normal,
+                          color: Color(0xFF0C1E10),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Color(0xFF1E90FF).withOpacity(0.3),
+                  ),
+                  margin: EdgeInsets.symmetric(horizontal: 10),
+                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 4),
+                  alignment: Alignment.center,
+                  child: const Text(
+                    'calmo',
+                    style: TextStyle(
+                      fontFamily: 'Axiforma',
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF1E90FF),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.symmetric(horizontal: 10),
+                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 4),
+                  alignment: Alignment.topLeft,
+                  child: Row(
+                    children: [
+                      SvgPicture.asset('../assets/heart.svg',
+                          height: 22, width: 22),
+                      SizedBox(width: 10), // espaço entre o ícone e o texto
+                      Text(
+                        '48 BPM',
+                        style: TextStyle(
+                          fontFamily: 'Axiforma',
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF0C1E10),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+} 
+
+
+/* Modal ---------------------------------------------------------
 class ModalSheet extends StatefulWidget {
   @override
   _ModalSheetState createState() => _ModalSheetState();
@@ -68,6 +191,7 @@ class _ModalSheetState extends State<ModalSheet> {
                               4.0, // Espaçamento vertical entre as linhas
                           children: [
                             const Text(
+                              textAlign: TextAlign.center,
                               'Aproxime o celular da tag para cadastrar o dispositivo.',
                               style: TextStyle(
                                 fontFamily: 'Axiforma',
@@ -78,16 +202,6 @@ class _ModalSheetState extends State<ModalSheet> {
                             ),
                           ],
                         ),
-                        /*
-                        child: const Text(
-                          'Aproxime o celular da tag para cadastrar o dispositivo.',
-                          style: TextStyle(
-                            fontFamily: 'Axiforma',
-                            fontSize: 16,
-                            fontWeight: FontWeight.normal,
-                            color: Color(0xFF0C1E10),
-                          ),
-                        ),*/
                       ),
                       ButtonCustom(
                         onPressed: () {},
@@ -108,6 +222,7 @@ class _ModalSheetState extends State<ModalSheet> {
     );
   }
 }
+*/
 
 /* Navbar ---------------------------------------------------------
 class MyHomePage extends StatefulWidget {
